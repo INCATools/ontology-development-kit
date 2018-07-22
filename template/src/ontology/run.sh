503047b957feda8e1931c3ea9f5b03493b03b20b
cmungall <cjm@berkeleybop.org>
2018-07-22 16:58:45 -0700

    use oskfull

--- template/src/ontology/run.sh
+++ template/src/ontology/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/osklite "$@"
+docker run -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/oskfull "$@"
