faa021165a111bcea9ca1e5f5fb322cbc6f761fe
cmungall <cjm@berkeleybop.org>
2018-08-03 15:57:20 -0700

    Experiment with osklite

--- seed-via-docker.sh
+++ seed-via-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -v $PWD:/work -w /work --rm -ti obolibrary/oskfull ./seed-my-ontology-repo.pl -e obo-ci-reports-all@groups.io "$@"
+docker run -v $PWD:/work -w /work --rm -ti obolibrary/osklite ./seed-my-ontology-repo.pl -e obo-ci-reports-all@groups.io "$@"
