1755ec6696c2dad3c60c1eb6d98280e2e33d961f
matentzn <nicolas.matentzoglu@gmail.com>
2019-02-26 10:39:20 +0000

    Fixed bat script

--- seed-via-docker.bat
+++ seed-via-docker.bat
@@ -1,2 +1,2 @@
 ::BATCH file to seed new ontology repo via docker
-docker run -v %cd%:/work -w /work --rm -ti obolibrary/odkfull ./odk/odk.py %*
+docker run -v %userprofile%/.gitconfig:/root/.gitconfig -v %cd%:/work -w /work --rm -ti obolibrary/odkfull /tools/odk.py seed %*
