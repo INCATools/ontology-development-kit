65a66e1be97fa08f21c011562c117750c8ab75c4
matentzn <nicolas.matentzoglu@gmail.com>
2019-11-01 07:23:53 +0000

    Update upgrade_repos_test.sh

--- upgrade_repos_test.sh
+++ upgrade_repos_test.sh
@@ -15,3 +15,4 @@ ROOTDIR_ALT=/Volumes/Nico-EBI/odk_repos_update
 
 ./upgrade-via-docker.sh dpo $CONFIGDIR/dpo-odk.yaml $ROOTDIR/drosophila-phenotype-ontology
 ./upgrade-via-docker.sh mp $CONFIGDIR/mp-odk.yaml $ROOTDIR/mammalian-phenotype-ontology
+./upgrade-via-docker.sh hp $CONFIGDIR/hp-odk.yaml $ROOTDIR/human-phenotype-ontology
