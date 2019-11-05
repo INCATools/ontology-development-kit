878ec33e91426e21ba808fc6be07bf306d88730b
matentzn <nicolas.matentzoglu@gmail.com>
2019-11-05 23:07:15 +0000

    Update upgrade_repos_test.sh

--- upgrade_repos_test.sh
+++ upgrade_repos_test.sh
@@ -6,13 +6,12 @@
 
 set -e
 
-make docker-build-use-cache
+#make docker-build-use-cache
 #docker pull obolibrary/odkfull
 
 CONFIGDIR=configs
 ROOTDIR=~/ws
 ROOTDIR_ALT=/Volumes/Nico-EBI/odk_repos_update
 
-./upgrade-via-docker.sh dpo $CONFIGDIR/dpo-odk.yaml $ROOTDIR/drosophila-phenotype-ontology
-./upgrade-via-docker.sh mp $CONFIGDIR/mp-odk.yaml $ROOTDIR/mammalian-phenotype-ontology
-./upgrade-via-docker.sh hp $CONFIGDIR/hp-odk.yaml $ROOTDIR/human-phenotype-ontology
+#./upgrade-via-docker.sh hp $CONFIGDIR/hp-odk.yaml $ROOTDIR/human-phenotype-ontology
+./upgrade-via-docker.sh mo $CONFIGDIR/mo-odk.yaml $ROOTDIR/monarch-ontology
