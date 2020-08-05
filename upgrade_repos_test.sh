1d5c07d748a14be70279f4c1788647a2057f7a40
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2020-08-05 21:54:36 +0100

    Update upgrade_repos_test.sh

--- upgrade_repos_test.sh
+++ upgrade_repos_test.sh
@@ -26,8 +26,11 @@ ROOTDIR_ALT=/Volumes/Nico-EBI/odk_repos_update
 #./upgrade-via-docker.sh phipo $CONFIGDIR/phipo-odk.yaml $ROOTDIR/phipo
 #./upgrade-via-docker.sh ecto $CONFIGDIR/ecto-odk.yaml $ROOTDIR/environmental-exposure-ontology
 #./upgrade-via-docker.sh wbphenotype $CONFIGDIR/wbphenotype-odk.yaml $ROOTDIR/c-elegans-phenotype-ontology
-./upgrade-via-docker.sh wbls $CONFIGDIR/wbls-odk.yaml $ROOTDIR/c-elegans-development-ontology
-./upgrade-via-docker.sh wbbt $CONFIGDIR/wbbt-odk.yaml $ROOTDIR/c-elegans-gross-anatomy-ontology
+#./upgrade-via-docker.sh wbls $CONFIGDIR/wbls-odk.yaml $ROOTDIR/c-elegans-development-ontology
+#./upgrade-via-docker.sh wbbt $CONFIGDIR/wbbt-odk.yaml $ROOTDIR/c-elegans-gross-anatomy-ontology
+#./upgrade-via-docker.sh cl $CONFIGDIR/cl-odk.yaml ~/knocean/cell-ontology
+#./upgrade-via-docker.sh zp $CONFIGDIR/zp-odk.yaml ~/ws/zebrafish-phenotype-ontology
+./upgrade-via-docker.sh pato $CONFIGDIR/pato-odk.yaml ~/ws/pato
 #./seed-via-docker.sh -c -g False -C $CONFIGDIR/wbphenotype-odk.yaml
 #./upgrade-via-docker.sh dpo $CONFIGDIR/dpo-odk.yaml $ROOTDIR/drosophila-phenotype-ontology
 #./upgrade-via-docker.sh geno $CONFIGDIR/geno-odk.yaml $ROOTDIR/GENO-ontology
