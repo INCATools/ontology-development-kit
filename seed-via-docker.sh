a8c562ed02fa03f3bc7f41d915a6a5a0ae50433b
Damien Goutte-Gattat <dpg44@cam.ac.uk>
2021-05-19 09:51:28 +0100

    Don't run the seeding script in an interactive container.
    Trying to run the seeding script interactively fails on Github Actions
    because the container has no TTY.

--- seed-via-docker.sh
+++ seed-via-docker.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-docker run -v $HOME/.gitconfig:/root/.gitconfig -v $PWD:/work -w /work --rm -ti obolibrary/odkfull /tools/odk.py seed "$@"
+docker run -v $HOME/.gitconfig:/root/.gitconfig -v $PWD:/work -w /work --rm obolibrary/odkfull /tools/odk.py seed "$@"
