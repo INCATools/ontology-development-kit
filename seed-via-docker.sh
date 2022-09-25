465455033ea5b7d288cb38e56450f1db778d030c
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2022-09-25 18:01:06 +0300

    Update seed-via-docker.sh

--- seed-via-docker.sh
+++ seed-via-docker.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-ODK_IMAGE=${ODK_IMAGE:-odkfull}
-ODK_TAG=${ODK_TAG:-latest}
-ODK_GITNAME=${ODK_GITNAME:-$(git config --get user.name)}
-ODK_GITEMAIL=${ODK_GITEMAIL:-$(git config --get user.email)}
+IMAGE=${IMAGE:-odkfull}
 
-docker run -u $(id -u):$(id -g) -v $PWD:/work -w /work --rm obolibrary/$ODK_IMAGE:$ODK_TAG /tools/odk.py seed --gitname "$ODK_GITNAME" --gitemail "$ODK_GITEMAIL" "$@"
+docker run -v $HOME/.gitconfig:/root/.gitconfig -v $PWD:/work -w /work --rm obolibrary/$IMAGE /tools/odk.py seed "$@"
