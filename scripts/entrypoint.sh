249a8d7c58be22670680d3720b44c9e69d107a18
Damien Goutte-Gattat <dpg44@cam.ac.uk>
2023-01-06 14:22:38 +0000

    Create a 'odkuser' user account in the image's entrypoint.
    Add an entrypoint script to the odklite (and thus also odkfull) image.
    This script will create a new user account called `odkuser` with a
    specific UID and GID, and then run whatever command has been specified
    in the `docker run` call under that new account.

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REAL_USER_ID=${ODKUSER_USER_ID:-1000}
+REAL_GROUP_ID=${ODKUSER_GROUP_ID:-1000}
+
+groupadd -o -g $REAL_GROUP_ID odkuser
+useradd -o -s /bin/bash -u $REAL_USER_ID -g $REAL_GROUP_ID -c "ODK User" -m odkuser
+PATH=$PATH:/home/odkuser/.local/bin
+
+exec sudo -H -E -u odkuser "$@"
