46617772ef155b7923135b2786a5a4f3317ff1e7
Damien Goutte-Gattat <dpg44@cam.ac.uk>
2023-01-07 19:08:26 +0000

    Make sure we can write to /work.
    Make sure the /work directory exists and belongs to the ODK user.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,5 +6,7 @@ REAL_GROUP_ID=${ODKUSER_GROUP_ID:-1000}
 groupadd -o -g $REAL_GROUP_ID odkuser
 useradd -o -s /bin/bash -u $REAL_USER_ID -g $REAL_GROUP_ID -c "ODK User" -m odkuser
 PATH=$PATH:/home/odkuser/.local/bin
+[ -d /work ] || mkdir /work
+chown -R odkuser:odkuser /work
 
 exec sudo -H -E -u odkuser "$@"
