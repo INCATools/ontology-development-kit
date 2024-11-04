fdbd240cfd129bb82dbe00e2bd7b51cdab68579d
Damien Goutte-Gattat <dpg44@cam.ac.uk>
2024-11-04 19:03:09 +0000

    Compute Python constraints from a bare image.
    To compute the set of Python constraints, we install all the Python
    packages we need in a bare Ubuntu image (same version as the one used to
    build the ODK itself) in which we only install Python3 and PIP.
    
    This ensure that `pip-constraints.txt`, the list of Python packages that
    are installed by the system and that PIP should not touch no matter
    what, always reflect the actual list of system-managed Python packages
    found in the ODK.
    
    This has the side-effect of allowing to use the `update-constraints.sh`
    script for the specific case of bootstrapping the constraints when the
    base image is upgraded to a newer version of Ubuntu, something that
    previously necessitated an undocumented manual operation.

--- update-constraints.sh
+++ update-constraints.sh
@@ -2,23 +2,35 @@
 
 set -e
 
-if [ -d /usr/lib/python3/dist-packages ]; then
-    # For any Python package already provided by the system, we must
-    # force PIP to use the exact same version as the one installed
-    find /usr/lib/python3/dist-packages -type d -name '*-info' | \
-        sed -E 's,/usr/lib/python3/dist-packages/(.+)-([^-]+)\.(egg|dist)-info,\1==\2,' \
-        > pip-constraints.txt
-fi
+in_docker=0
+[ "x$1" = x--in-docker ] && in_docker=1
 
-if [ "x$1" = x--install-virtualenv ]; then
+if [ $in_docker -eq 1 ]; then
+    # Install the same base Python packages as the one present in the
+    # ODK builder image.
     apt-get update
-    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends python3-virtualenv
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+        python3-dev python3-pip
+
+    # Get the list of the Python packages that were actually installed
+    # so we can instruct PIP to use the same versions of said packages.
+    find /usr/lib/python3/dist-packages -type d -name '*-info' | \
+        sed -E 's,/usr/lib/python3/dist-packages/(.+)-([^-]+)\.(egg|dist)-info,\1==\2,' | \
+        sort > pip-constraints.txt
+
+    # Now additionally install virtualenv, which we will need to
+    # install all ODK packages in a separate environment.
+    apt-get install -y --no-install-recommends python3-virtualenv
+
+    # Make sure we are using below the same version of Python as the
+    # one in the ODK
+    PYTHON_VERSION=$(python3 --version | sed -E 's,^Python 3\.([0-9]+)\.[0-9]+$,3.\1,')
 fi
 
-virtualenv tmpdir
+# The version of Python should match
+virtualenv -p ${PYTHON_VERSION:-3.12} tmpdir
 . tmpdir/bin/activate
-export PIP_CONSTRAINT=$(pwd)/pip-constraints.txt
-python3 -m pip install -U pip
+[ -f pip-constraints.txt ] && export PIP_CONSTRAINT=$(pwd)/pip-constraints.txt
 python3 -m pip install -r requirements.txt.full
 python3 -m pip freeze > constraints.txt
 
