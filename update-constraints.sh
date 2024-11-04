9dba99a128670fecd2efc9cc2dbf730d821b85ca
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2024-11-04 16:51:08 +0200

    Avoid adding setuptools to constraints.txt
    This is a bit of a hack, but I cant imagine it being a problem, as during ODK install https://github.com/INCATools/ontology-development-kit/commit/f1bdb983e2ae0128e7a8fbfb60b7c05755c4c5d0 kicks in.
    
    However, it might create some non-deterministic behaviour in the process.. not sure.

--- update-constraints.sh
+++ update-constraints.sh
@@ -11,6 +11,9 @@ virtualenv tmpdir
 . tmpdir/bin/activate
 python3 -m pip install -U pip
 python3 -m pip install -r requirements.txt.full
-python3 -m pip freeze > constraints.txt
+
+# For some context of this see here: 
+# https://github.com/INCATools/ontology-development-kit/commit/f1bdb983e2ae0128e7a8fbfb60b7c05755c4c5d0
+python3 -m pip freeze | grep -v "setuptools" > constraints.txt
 
 test -n "$(head constraints.txt)"
