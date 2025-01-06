beb3db155df31aa4ee901cb06c3f37893a9b86b9
Damien Goutte-Gattat <dpg44@cam.ac.uk>
2025-01-06 19:33:05 +0000

    Make sure /tools/.coursier-cache is user-writeable.
    The ODKFull image defines the COURSIER_CACHE environment variable to
    /tools/.coursier-cache, so Ammonite and Scala-CLI expect to be able to
    store their downloaded dependencies in there.
    
    If we run under a non-privileged user, we need to ensure that, if the
    directory does not exist, it is created and given to the odkuser.
    
    (If the directory already exists, it means a host directory has been
    explicitly bound to it, in which case permissions will already be
    correct, and we have nothing to do.)

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -13,6 +13,10 @@ mkdir -p /home/odkuser
 chown odkuser:odkuser /home/odkuser
 [ -d /home/odkuser/.data ] && chown odkuser:odkuser /home/odkuser/.data
 [ -d /home/odkuser/.data/oaklib ] && chown odkuser:odkuser /home/odkuser/.data/oaklib
+if [ ! -d /tools/.coursier-cache ] ; then
+    mkdir /tools/.coursier-cache
+    chown odkuser:odkuser /tools/.coursier-cache
+fi
 PATH=$PATH:/home/odkuser/.local/bin
 [ -S /run/host-services/ssh-auth.sock ] && chown odkuser /run/host-services/ssh-auth.sock
 
