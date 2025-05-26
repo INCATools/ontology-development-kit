8d958b6ba8234744dce7e3f6279d4d40ff957bc0
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2025-05-26 18:30:19 +0100

    Make the tests fail when a ODK program can't be run.
    Update the test-program.sh script so that it not only prints a "KO"
    string when the test program cannot be run, but it also exists with a
    non-zero exit code, so that it makes the `test_odk{full|lite}_programs`
    target fail.
    
    closes #1258

--- tests/test-program.sh
+++ tests/test-program.sh
@@ -5,4 +5,4 @@ ODK_TAG=${ODK_TAG:-latest}
 
 program_name=$1; shift
 echo -n "Checking for $program_name... "
-docker run --rm obolibrary/$ODK_IMAGE:$ODK_TAG $@ >/dev/null && echo OK || echo KO
+docker run --rm obolibrary/$ODK_IMAGE:$ODK_TAG $@ >/dev/null && echo OK || { echo KO ; exit 1 ; }
