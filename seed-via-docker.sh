de13c6a071079f9bdc8279fb6bac510d729e7116
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2025-12-24 11:43:10 +0000

    Add odk.py symlinks for compatibility.
    The main `odk` script used to be located at `/tools/odk.py`, where it
    could be invoked either as `/tools/odk.py` (full path, hardcoded) or as
    `odk.py` (since `/tools` was in the PATH).
    
    Now the script lives in `/usr/local/bin/odk`, and is expected to be
    invoked simply with `odk`.
    
    But existing calls to either `/tools/odk.py` or `odk.py` should still
    work for compatibility. In particular, the `seed-via-docker.sh` script
    must be usable with both pre-1.7 ODK images (where the script is under
    `/tools/odk.py`) and post-1.7 images.
    
    To ensure that, we add symlinks to the image so that `/tools/odk.py` and
    `/usr/local/bin/odk.py` both point to `/usr/local/bin/odk`.

--- seed-via-docker.sh
+++ seed-via-docker.sh
@@ -9,4 +9,4 @@ ODK_TAG=${ODK_TAG:-latest}
 ODK_GITNAME=${ODK_GITNAME:-$(git config --get user.name)}
 ODK_GITEMAIL=${ODK_GITEMAIL:-$(git config --get user.email)}
 
-docker run -e ODK_USER_ID=$(id -u) -e ODK_GROUP_ID=$(id -g) -v $PWD:/work -w /work --rm obolibrary/$ODK_IMAGE:$ODK_TAG odk seed --gitname "$ODK_GITNAME" --gitemail "$ODK_GITEMAIL" "$@"
+docker run -e ODK_USER_ID=$(id -u) -e ODK_GROUP_ID=$(id -g) -v $PWD:/work -w /work --rm obolibrary/$ODK_IMAGE:$ODK_TAG /tools/odk.py seed --gitname "$ODK_GITNAME" --gitemail "$ODK_GITEMAIL" "$@"
